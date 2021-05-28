e460dc7ab5e75e2f8f36fd79369a1b7c4ed2e2d2
Qi Wang <q.wang@efiport.de>
2021-05-28 12:55:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import env from "dotenv";
+env.config();
 
 class App extends Component {
   render() {
@@ -12,7 +14,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.  test hook!!111111
-        </p>
+        </p> `test secret is: ${environment}`
       </div>
     );
   }
